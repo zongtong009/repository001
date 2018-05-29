c92d9108985c63ad566b31d65190bc5d0ddd7152
zongtong009 <zongtongguilai1@gmail.com>
2018-05-29 14:14:39 +0800

    first test
    第一次上传

---
+++ 020随机数.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr 13 18:09:29 2018
+
+@author: Administrator
+"""
+import random
+random.seed(10)   #不用种子会使用系统时间
+for i in range(20):  
+    print(random.random())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ sleep.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 28 21:44:10 2018
+
+@author: Administrator
+"""
+import time
+def wait():
+    time.sleep(3.3)
+for i in range(50):
+    wait
+    print(i)
